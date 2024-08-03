cf9f475fa0e73b29eeb39b106739a0d36b36bd8e
Geon.C <chlrjs1324@gmail.com>
2024-08-03 09:32:51 +0900

    Initial commit

---
+++ src/test/java/com/core/fintechbackend/FintechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.fintechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
