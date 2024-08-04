3bba3de6649f2a01609c7e0d97d653a5469bd832
Geon.C <chlrjs1324@gmail.com>
2024-08-04 20:53:22 +0900

    chore : 미사용 코드 삭제

--- src/main/java/com/core/backend/common/mock/MockData.java
+++ src/main/java/com/core/backend/common/mock/MockData.java
@@ -1,9 +1,6 @@
 package com.core.backend.common.mock;
 
 import com.core.backend.group.ui.dto.GroupInfoResponse;
-import com.core.backend.group.ui.dto.GroupMemberResponse;
-import com.core.backend.group.ui.dto.GroupSettlementListResponse;
-import com.core.backend.group.ui.dto.GroupSettlementResponse;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -28,34 +25,5 @@ public class MockData {
         );
 
         return List.of(response1, response2, response3, response4, response5);
-    }
-
-    public static GroupSettlementResponse getGroupSettlementMockData() {
-
-        GroupMemberResponse memberResponse1 = new GroupMemberResponse(
-                1L, "민선"
-        );
-        GroupMemberResponse memberResponse2 = new GroupMemberResponse(
-                2L, "나은"
-        );
-        GroupMemberResponse memberResponse3 = new GroupMemberResponse(
-                3L, "해성"
-        );
-        GroupMemberResponse memberResponse4 = new GroupMemberResponse(
-                4L, "한비"
-        );
-        GroupMemberResponse memberResponse5 = new GroupMemberResponse(
-                5L, "나영"
-        );
-        GroupMemberResponse memberResponse6 = new GroupMemberResponse(
-                6L, "건"
-        );
-
-        List<GroupMemberResponse> memberResponseList = List.of(memberResponse1, memberResponse2, memberResponse3, memberResponse4, memberResponse5, memberResponse6);
-
-        GroupSettlementListResponse settlementResponse = new GroupSettlementListResponse(
-
-        );
-
     }
 }
