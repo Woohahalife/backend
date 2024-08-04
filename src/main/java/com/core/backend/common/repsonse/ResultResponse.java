64b793f7631e9f39c6646c2988c14240280aabab
Geon.C <chlrjs1324@gmail.com>
2024-08-04 14:58:36 +0900

    modify : HttpStatus ResultResponse 반영

--- src/main/java/com/core/backend/common/repsonse/ResultResponse.java
+++ src/main/java/com/core/backend/common/repsonse/ResultResponse.java
@@ -10,29 +10,29 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@JsonPropertyOrder({"statusCode", "message", "status", "data"})
+@JsonPropertyOrder({"status","statusCode", "message", "status", "data"})
 public final class ResultResponse<T> {
 
+    private HttpStatus status;
     private int statusCode;
     private String message;
-    private String status;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
     public static <T> ResultResponse<T> success(T data) {
         return new ResultResponse<>(
+                HttpStatus.OK,
                 HttpStatus.OK.value(),
                 "요청이 성공적으로 처리되었습니다.",
-                HttpStatus.OK.name(),
                 data);
     }
 
     public static <T> ResultResponse<T> failure(ErrorCode errorCode) {
         return new ResultResponse<>(
+                errorCode.getHttpStatus(),
                 errorCode.getStatusCode(),
                 errorCode.getMessage(),
-                errorCode.name(),
                 null);
     }
 }
