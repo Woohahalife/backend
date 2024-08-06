4bf736bf75c91ce35a0731421e3bf1e4b4c31abf
Geon.C <chlrjs1324@gmail.com>
2024-08-07 02:30:32 +0900

    feat : interceptor, resolver 등록

--- src/main/java/com/core/backend/config/WebConfig.java
+++ src/main/java/com/core/backend/config/WebConfig.java
@@ -1,9 +1,34 @@
 package com.core.backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
+import com.core.backend.auth.config.AuthArgumentResolver;
+import com.core.backend.auth.config.AuthInterceptor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+	private final AuthInterceptor authInterceptor;
+	private final AuthArgumentResolver authArgumentResolver;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/**/auth/**")
+			.excludePathPatterns("/users/signup");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(authArgumentResolver);
+	}
 }
