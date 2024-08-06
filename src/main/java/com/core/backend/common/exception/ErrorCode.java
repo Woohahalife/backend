cedf961b882c2ffe46fb733510e08092a5e693f4
Geon.C <chlrjs1324@gmail.com>
2024-08-06 21:24:43 +0900

    modify : SoftDelete 체크용 ErrorCode 작성

--- src/main/java/com/core/backend/common/exception/ErrorCode.java
+++ src/main/java/com/core/backend/common/exception/ErrorCode.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+
+    ALREADY_DELETED_DATA(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.value(), "이미 삭제처리 된 데이터입니다."),
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.value(), "inertnal_server_error");
 
     private final HttpStatus httpStatus;
