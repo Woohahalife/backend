e9b1ef5ad637c97aa9ed23c02c0d83417eba2aa3
Geon.C <chlrjs1324@gmail.com>
2024-08-09 05:08:10 +0900

    chore : 기타 코드 수정
    - dto 변수타입 일부 수정 및 TODO 추가

--- src/main/java/com/core/backend/settlement/ui/dto/SettlementRegisterRequest.java
+++ src/main/java/com/core/backend/settlement/ui/dto/SettlementRegisterRequest.java
@@ -10,12 +10,12 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class SettlementRegisterRequest {
 
     private Long id; // mockData 저장을 위한 임의 프로퍼티
     private String settlementName;
-    private int totalAmount;
+    private Long totalAmount;
     private LocalDate groupingAt;
     private LocalDate settlementAt;
     private String settlementPlace;
