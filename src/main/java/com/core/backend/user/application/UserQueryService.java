76b4e0ca9f62f3d566c9afe50dc234a9bc142925
Geon.C <chlrjs1324@gmail.com>
2024-08-09 15:49:42 +0900

    feat : 사용자 마이페이지 조회 API 작성

--- src/main/java/com/core/backend/user/application/UserQueryService.java
+++ src/main/java/com/core/backend/user/application/UserQueryService.java
@@ -1,12 +1,23 @@
 package com.core.backend.user.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.core.backend.user.domain.repository.UserRepository;
+import com.core.backend.user.ui.dto.UserMyPageResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserQueryService {
+
+	private final UserRepository userRepository;
+
+	public UserMyPageResponse getMyPage(Long userId) {
+		return UserMyPageResponse.from(userRepository.findById(userId));
+	}
 }
