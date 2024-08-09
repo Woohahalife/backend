eabd519aca1d5af632907b54810ea02b08286166
Geon.C <chlrjs1324@gmail.com>
2024-08-09 19:22:25 +0900

    modify : 무결성 제약조건 위반 exceptionHandler 작성

--- src/main/java/com/core/backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/core/backend/common/exception/GlobalExceptionHandler.java
@@ -1,19 +1,20 @@
 package com.core.backend.common.exception;
 
-import com.core.backend.common.repsonse.ResultResponse;
-
-import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.core.backend.common.repsonse.ResultResponse;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(DefaultException.class)
-	public ResponseEntity<ResultResponse<Object>> defaultExceptionHandler(DefaultException e) {
+	public ResponseEntity<Object> defaultExceptionHandler(DefaultException e) {
 		log.error("[Exception] status: {} message: {} kind: {}", e.getErrorCode().getHttpStatus(), e.getMessage(),
 			e.getClass().getSimpleName());
 
@@ -26,5 +27,12 @@ public class GlobalExceptionHandler {
 
 		return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatusCode())
 			.body(ResultResponse.failure(ErrorCode.INTERNAL_SERVER_ERROR));
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+			log.error("[409 : CLIENT_CONFLICT_ERROR]", e);
+			return ResponseEntity.status(ErrorCode.CONFLICT.getStatusCode())
+				.body(ResultResponse.failure(ErrorCode.CONFLICT));
 	}
 }
