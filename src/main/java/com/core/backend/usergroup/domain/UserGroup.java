bec6f76f3b8de1ebdfad2fbe4da01465f100e5bf
Geon.C <chlrjs1324@gmail.com>
2024-08-10 14:55:36 +0900

    modify : userGroup unique 제약조건 추가

--- src/main/java/com/core/backend/usergroup/domain/UserGroup.java
+++ src/main/java/com/core/backend/usergroup/domain/UserGroup.java
@@ -12,13 +12,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "user_group")
+@Table(name = "user_group", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "group_id"}))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserGroup extends BaseEntity {
 
